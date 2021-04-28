5edf054a1758feb9216c72e5c5f2b317b1c3f0e6
Thanaphat-path <6206021611028@fitm.kmutnb.ac.t>
2021-04-28 17:16:25 +0700

    update

--- pro/show/main.js
+++ pro/show/main.js
@@ -1,11 +1,19 @@
 let apiUser ="http://127.0.0.1:5000/api/users"
+let apimovie ="http://127.0.0.1:5000/api/name-movie"
+
 let setUser = document.querySelector(".set-user");
-
+let setmovie = document.querySelector(".set-movie");
 getUser()
-
+getmovie()
 async function getUser(){
     let i =0
     let orderObj = await axios.get(apiUser)
     setUser.innerHTML = orderObj.data[i].username
+
+}
+async function getmovie(){
+    let i =0
+    let orderObj = await axios.get(apimovie)
+    setmovie.innerHTML = orderObj.data[i].name
 
 }
