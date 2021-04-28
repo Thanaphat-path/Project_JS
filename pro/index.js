1c29b6332b2cf68343a62c400a7d94a9b83a8956
Thanaphat-path <6206021611028@fitm.kmutnb.ac.t>
2021-04-28 11:55:48 +0700

    new

--- pro/index.js
+++ pro/index.js
@@ -4,28 +4,16 @@ const path = require("path")
 const fs = require('fs')
 const bodyParser = require('body-parser')
 const { Mongoose } = require('mongoose')
-const mongo =require('mongo')
+//const mongo =require('mongo')
 const userSchema =require('schemas/user_schemas')
 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  const filename = path.join(__dirname, 'index1.html')
+  const filename = path.join(__dirname, 'index.html')
   const indexData = fs.readFileSync(filename)
   res.end(indexData)
 })
-
-const connectToMongoDB= async ()=>{
-  await mongo().then( async(Mongoose) =>{
-    try{
-      console.log('Conmected to mongodb!')
-
-    } finally{
-      Mongoose.connection.close()
-    }
-  })
-}
-connectToMongoDB()
 
 app.listen(3000, function () {
   console.log("server running on port 3000");
