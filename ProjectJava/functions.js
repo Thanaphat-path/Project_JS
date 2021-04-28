136d575e390d55f813ada91079b2c951f1d65936
pattaramon <6206021621121@fitm.kmutnb.ac.th>
2021-04-29 00:10:41 +0700

    meiji

--- ProjectJava/functions.js
+++ ProjectJava/functions.js
@@ -1,22 +1,15 @@
-let apiUser ="http://127.0.0.1:5000/api/users"
 let apimovie ="http://127.0.0.1:5000/api/name-movie"
 
-let setUser = document.querySelector(".set-user");
 let setmovie = document.querySelector(".set-movie");
-getUser()
+
 getmovie()
-async function getUser(){
-    let i =0
-    let orderObj = await axios.get(apiUser)
-    function nextuser(){
-        setUser.innerHTML = orderObj.data[i].username
-    }
-    nextuser()
-}
+
 async function getmovie(){
     let i =0
+    console.log("kfsjgspor");
     var movieObj = await axios.get(apimovie)
-    let picture =movieObj.data[i].name
-    setmovie.innerHTML = picture
-
+    console.log(movieObj.data[i].name);
+    
+    setmovie.innerHTML = await movieObj.data[i].name
+    
 }
