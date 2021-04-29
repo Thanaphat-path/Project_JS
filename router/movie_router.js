53fd68f3dd5e35927c9bbdd23640a6d797aab71e
Thanaphat-path <6206021611028@fitm.kmutnb.ac.th>
2021-04-29 14:27:36 +0700

    up

--- router/movie_router.js
+++ router/movie_router.js
@@ -22,10 +22,10 @@ router.get("/:_id", (req, res) => {
 // POST (create new data)
 router.post("/", (req, res) => {
   const objForInsert = new movie({
-    name: req.body.name,
-    img: req.body.imgName,
+    name: req.body,
+    img: req.body,
     vdo_ex: req.body,
-    vdo_main: req.body.vdo_main,
+    vdo_main: req.body,
   });
 
   objForInsert.save((err, data) => {
