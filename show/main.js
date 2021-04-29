3fed7a7150bcfdc34763d45573060f6e098978c4
Thanaphat-path <6206021611028@fitm.kmutnb.ac.th>
2021-04-29 08:42:00 +0700

    q

--- show/main.js
+++ show/main.js
@@ -8,8 +8,11 @@ let setmovie3 = document.querySelector(".set-movie3");
 let setmovie4 = document.querySelector(".set-movie4");
 let setmovie5 = document.querySelector(".set-movie5");
 getUser()
-getmovie()
-
+getmovie1()
+getmovie2()
+getmovie3()
+getmovie4()
+getmovie5()
 async function getUser(){
     let i =0
     let orderObj = await axios.get(apiUser)
@@ -19,39 +22,33 @@ async function getUser(){
     nextuser()
 }
 async function getmovie1(){
-    let i =0
     var movieObj = await axios.get(apimovie)
     let picture =movieObj.data[0].name
     setmovie1.innerHTML = picture
 
 }
-
 async function getmovie2(){
-    let i =0
+ 
     var movieObj = await axios.get(apimovie)
     let picture =movieObj.data[1].name
     setmovie2.innerHTML = picture
 
 }
-
 async function getmovie3(){
-    let i =0
+ 
     var movieObj = await axios.get(apimovie)
     let picture =movieObj.data[2].name
     setmovie3.innerHTML = picture
 
 }
-
 async function getmovie4(){
-    let i =0
     var movieObj = await axios.get(apimovie)
     let picture =movieObj.data[3].name
     setmovie4.innerHTML = picture
 
 }
-
 async function getmovie5(){
-    let i =0
+
     var movieObj = await axios.get(apimovie)
     let picture =movieObj.data[4].name
     setmovie5.innerHTML = picture
